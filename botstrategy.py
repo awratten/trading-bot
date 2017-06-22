9791fba35de300e3f794d780bdb21241a3d9b304
kenfrost1987 <anthony.wratten@kenfrost1987.com.au>
2017-06-22 10:36:44 +1000

    update

--- botstrategy.py
+++ botstrategy.py
@@ -11,6 +11,7 @@ class BotStrategy(object):
 	def __init__(self):
 		self.output = BotLog()
 		self.prices = []
+		self.closes = [] #for Momentum
 		self.trades = []
 		self.currentPrice = None
 		self.numSimulTrades = 1
@@ -23,10 +24,17 @@ class BotStrategy(object):
 	def tick(self,candlestick):
 		self.currentPrice = float(candlestick['weightedAverage'])
 		self.currentVolume = float(candlestick['volume'])
-		self.prices.append(self.currentPrice)
-		
-		self.output.log("Price: "+str(candlestick['weightedAverage'])+"\tMoving Average: "+str(self.indicators.movingAverage(self.prices,15)))
 
+		self.open = float(candlestick['open'])
+		self.close = float(candlestick['close'])
+		self.high = float(candlestick['high'])
+		self.low = float(candlestick['low'])
+
+		self.prices.append(self.currentPrice)
+		self.closes.append(self.close) # for Momentum
+		
+		self.output.log("Price: "+str(candlestick['weightedAverage'])+"\tMoving Average: "+str(self.indicators.movingAverage(self.prices,15))+"\tMomentum: "+str(self.indicators.momentum(self.closes)))
+		
 		self.evaluatePositions()
 		self.updateOpenTrades()
 		self.showPositions()
@@ -41,6 +49,8 @@ class BotStrategy(object):
 		if (len(openTrades) < self.numSimulTrades):
 			#if (self.currentPrice < self.indicators.movingAverage(self.prices,15)):
 			#if (float(ticker['BTC_ZEC']['percentChange']) < 0):
+			print("Momentum: " + str(self.indicators.momentum(self.closes)))
+
 			if (self.indicators.trend(self.prices,self.trendPeriod) == 1 and self.currentVolume > self.minVolume):
 					self.trades.append(BotTrade(self.currentPrice,stopLoss=self.stopLoss))
 
