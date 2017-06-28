e12e734ad8d17610a95fd92b36ddd0a9ced6e61d
Anthony Wratten <awratten1@gmail.com>
2017-06-29 08:02:52 +1000

    Create getData.py

---
+++ getData.py
@@ -0,0 +1,25 @@
+from poloniex import Poloniex
+import time, datetime
+
+csvfile = open("btc_zec.csv", "a")
+
+
+conn = Poloniex()
+
+security = 'BTC_ZEC'
+period = 300
+Days = 230
+
+startTime = time.time() - (86400 * Days)
+endTime = time.time() #End at current time
+
+data = conn.returnChartData(security, period, startTime, endTime)
+
+def datetime_from_timestamp(timestamp):
+	x = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+	return x
+
+
+for tick in data:
+	print(str(datetime_from_timestamp(tick['date'])) + "," + tick['close'], file=csvfile)
+
