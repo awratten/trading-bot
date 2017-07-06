c7b752b508cf19b1d7f5131823d87f1924bd6284
Anthony Wratten <awratten1@gmail.com>
2017-07-06 11:47:54 +1000

    add EMA
    Fixed problem with internal referencing of functions. (had to add self.EMA and self.movingAverage)
    
    Should work properly now.

--- part 3/botindicators.py
+++ part 3/botindicators.py
@@ -42,3 +42,18 @@ class BotIndicators(object):
 		else:
 			return 50 # output a neutral amount until enough prices in list
 
+	
+	def EMA(self, dataPoints, period, position=None, previous_ema=None):
+		"""https://www.oanda.com/forex-trading/learn/forex-indicators/exponential-moving-average"""
+		if (len(dataPoints) < period + 2):
+			return None
+		c = 2 / float(period + 1)
+		if not previous_ema:
+			return self.EMA(dataPoints, period, period, self.movingAverage(dataPoints[-period*2 + 1:-period + 1], period))
+		else:
+			current_ema = (c * dataPoints[-position]) + ((1 - c) * previous_ema)
+			if position > 0:
+				return self.EMA(dataPoints, period, position - 1, current_ema)
+		return previous_ema
+	
+	
